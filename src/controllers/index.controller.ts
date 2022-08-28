f4d3da73931a602bc2d4576d324523d860995ee5
Hoang Trung <hoangtrung1801.2003@gmail.com>
2022-08-28 12:03:38 +0700

    feat: 🎸 make prisma schema

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
     public index = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
         try {
-            res.status(200);
+            res.sendStatus(200);
         } catch (error) {
             next(error);
         }
